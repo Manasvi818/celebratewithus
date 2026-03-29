5e9e1dd70448591908cdcb6bf3f808a9a01629ad
Manasvi <manusgaikwad@gmail.com>
2026-03-29 20:36:43 +0530

    fix razorpay verification issue

--- scripts/payment.js
+++ scripts/payment.js
@@ -48,7 +48,9 @@ async function openCheckout() {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      ...response,
+  razorpay_order_id: response.razorpay_order_id,
+  razorpay_payment_id: response.razorpay_payment_id,
+  razorpay_signature: response.razorpay_signature,
       name: localStorage.getItem("userName") || "Guest",
       email: localStorage.getItem("userEmail") || "guest@email.com",
       amount: 599,
