75e5ad8de852cb34514b448aae178771d562046f
Manasvi <manusgaikwad@gmail.com>
2026-04-01 21:48:14 +0530

    update editor page and viewer page and optimize.js

--- js/optimize.js
+++ js/optimize.js
@@ -1,10 +1,10 @@
 function optimizeCloudinaryUrl(url, width = 500) {
   if (!url || !url.includes("res.cloudinary.com")) return url;
 
-  if (url.includes("/upload/f_auto")) return url; // avoid duplicate
+  if (url.includes("/upload/f_auto")) return url;
 
   return url.replace(
     "/upload/",
-    `/upload/f_auto,q_auto,w_${width}/`
+    `/upload/f_auto,q_auto:eco,w_${width}/`
   );
 }
