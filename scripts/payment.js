771f0467b125a8c70cb7e8fa84ead47b8e754e34
Manasvi <manusgaikwad@gmail.com>
2026-04-10 22:14:06 +0530

    fix editor page and viewer page

--- scripts/payment.js
+++ scripts/payment.js
@@ -95,7 +95,7 @@ handler: async function (response) {
     amount: 149,
     name: "Guest",
     email: "guest@email.com",
-    template: selectedTemplate
+    template: selectedTemplate.toLowerCase().replace(/\s+/g, "-")
   })
 });
 
