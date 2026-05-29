19f7ed66d03bc98d49947001f51e9e12d536ff9e
Manasvi <manusgaikwad@gmail.com>
2026-05-29 22:43:45 +0530

    update CORS for Railway

--- scripts/payment.js
+++ scripts/payment.js
@@ -67,7 +67,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 razorpay_payment_id: response.razorpay_payment_id,
                 razorpay_order_id: response.razorpay_order_id,
                 razorpay_signature: response.razorpay_signature,
-                template
+                template,
+                coupon: appliedCoupon,         // ✅ add this
+  amountINR: finalAmount         // ✅ add this
               })
             });
 
